952e33121017b0c189cf58c3e7dbafb4365abbca
aaron <kopplina@mail.gvsu.edu>
2020-12-01 19:00:01 -0500

    adding more classes

---
+++ Player.py
@@ -0,0 +1,7 @@
+class Player:
+    def __init__(self, position: int, health: int, name: str, team: int):
+        self.position = position
+        self.health = health
+        self.name = name
+        self.team = team
+
