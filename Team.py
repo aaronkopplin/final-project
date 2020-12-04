f9787b7155ad2f6a0a94df1c64a98f1829fab7c8
Dylan <dprang01@gmail.com>
2020-12-03 19:46:57 -0500

    Slapper

--- Team.py
+++ Team.py
@@ -1,3 +1,4 @@
 class Team:
+    # Constructor for team class. Stores a list of each team's characters.
     def __init__(self, players: list):
-        self.players = players
+        self.players = players # Stores a list of characters, bruh.
